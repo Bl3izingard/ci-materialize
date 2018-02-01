51f8bb2b5a3f0bac3758ae699da6f5e6ec840b10
teola <teola@SHADOW-EFJG4KQ8>
2018-02-01 15:19:40 +0100

    Ajout Controleur Account

---
+++ base/application/views/account/signup.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html>
+	<head>
+		<title>Sign In</title>
+	</head>
+	<body>
+
+	<?php echo validation_errors(); ?>
+
+	<?php echo form_open('form'); ?>
+	
+	<form>
+
+	<h5>Pseudonyme</h5>
+	<input type="text" name="username" value="" size="50" />
+
+	<h5>Mot de passe</h5>
+	<input type="text" name="password" value="" size="50" />
+
+	<h5>Confirmation du mot de passe</h5>
+	<input type="text" name="passconf" value="" size="50" />
+
+	<h5>Adresse E-mail</h5>
+	<input type="text" name="email" value="" size="50" />
+
+	<div><input type="submit" value="Submit" /></div>
+
+	</form>
+
+	</body>
+</html>
