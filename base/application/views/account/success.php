7d20326f3db9e4087fb00b65848b9922f79564bb
teola <teola@SHADOW-EFJG4KQ8>
2018-02-01 16:00:15 +0100

    Texte pour le succes de connection

--- base/application/views/account/success.php
+++ base/application/views/account/success.php
@@ -4,11 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <html>
 	<head>
-		
+		<title>Success</title>
 	</head>
 	<body>
 
-	<h3>Your form was successfully submitted!</h3>
-
+	<div>
+		<h3>Your form was successfully submitted!</h3>
+	</div>
+	
 	</body>
 </html>
