efdf17ff012cc47005e54c1664b224311b73a4ba
Bl3izingard <jpierre@suid.fr>
2018-02-07 08:07:49 +0100

    Ajout des balises templates

--- base/application/views/template/header.php
+++ base/application/views/template/header.php
@@ -1,3 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<header>
+<!-- MENU PRINCIPAL (Non visible par les mobiles & tablette) -->
+
+<!-- MENU PRINCIPAL HAMBURGER (Non visible par les PCs) -->
+</header>
