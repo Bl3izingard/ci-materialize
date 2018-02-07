41a159c41c07efa9b28614478439c398a1d75af1
Bl3izingard <jpierre@suid.fr>
2018-02-07 08:28:00 +0100

    Ajout de JQuery & des fonts

--- base/application/views/template/footer.php
+++ base/application/views/template/footer.php
@@ -3,7 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <footer>
 </footer>
-<!-- Compiled and minified JavaScript -->
- <script src="<?php echo base_url("assets/js/materialize.min.js");?>"></script>
+	<!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	
+	<!-- Compiled and minified JavaScript -->
+ 	<script src="<?php echo base_url("assets/js/materialize.min.js");?>"></script>
 </body>
 </html>
--- base/application/views/template/head.php
+++ base/application/views/template/head.php
@@ -3,9 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <html>
 <head>
-<!-- Compiled and minified CSS -->
-  	<link rel="stylesheet" href="<?php echo base_url("assets/css/materialize.min.css");?>">
+	<!-- Compiled and minified CSS -->
+  	<link href="<?php echo base_url("assets/css/materialize.min.css");?>" rel="stylesheet">
+  	<link href="<?php echo base_url("assets/font/*");?>" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      
 	<title><?php echo $title_page; ?></title>
-  
+  	
+  	<!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
