9f8a2b9d1a57582e5845f165fd66ee6235ea7abd
Bl3izingard <jpierre@suid.fr>
2018-02-10 07:35:28 +0100

    Changement d'emplacement des fichiers pour plus de sécurité

---
+++ application/views/account/success.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html>
+	<head>
+		<title>Success</title>
+	</head>
+	<body>
+
+	<div>
+		<h3>Your form was successfully submitted!</h3>
+	</div>
+	
+	</body>
+</html>
