92dc502a9f0ab500d59762a78c7597fb965307f5
Bl3izingard <jpierre@suid.fr>
2018-02-10 14:40:34 +0100

    Design page signin

--- application/views/account/signin.php
+++ application/views/account/signin.php
@@ -2,15 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="container">
-	<form>
+	<div class="row">
+		<form class="col s12 m7 offset-m3 l6 offset-l3">
 
-	<h5>Login</h5>
-	<input type="text" name="username" value="" size="50" />
+		<h5>Login</h5>
+		<input type="text" name="username" value="" size="50" />
 
-	<h5>Password</h5>
-	<input type="text" name="password" value="" size="50" />
+		<h5>Password</h5>
+		<input type="text" name="password" value="" size="50" />
 
-	<button class="btn waves-effect waves-light" type="submit">Sign in <i class="material-icons right">send</i></button>
+		<button class="btn waves-effect waves-light" type="submit">
+			Sign in <i class="material-icons right">send</i>
+		</button>
 
-	</form>
+		</form>
+	</div>
 </div>
