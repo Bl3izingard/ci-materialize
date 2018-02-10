f5a3c2b74552197d4a649d452692b6660b7a4b4a
Bl3izingard <jpierre@suid.fr>
2018-02-10 07:31:17 +0100

    Ajout du menu non connecté

--- base/application/views/template/header.php
+++ base/application/views/template/header.php
@@ -2,19 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <header>
-<!-- MENU PRINCIPAL (Non visible par les mobiles & tablette) -->
+<!-- MENU PRINCIPAL (Non connnecté) -->
 <nav class="white" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="<?php echo base_url(); ?>">Home</a></li>
+        <li><a href="<?php echo base_url("signin"); ?>">Sign in</a></li>
+        <li><a href="<?php echo base_url("signup"); ?>">Sign up</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="<?php echo base_url(); ?>">Home</a></li>
+        <li><a href="<?php echo base_url("signin"); ?>">Sign in</a></li>
+        <li><a href="<?php echo base_url("signup"); ?>">Sign up</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
-  </nav>
-<!-- MENU PRINCIPAL HAMBURGER (Non visible par les PCs) -->
+ </nav>
+<!-- MENU PRINCIPAL (Connecté) -->
 </header>
